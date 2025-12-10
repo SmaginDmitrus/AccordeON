580592716c969bd1805af74e887c529b25f72f7a
SmaginDmitrus <smagindm2002@gmail.com>
2025-12-10 14:20:57 +0300

    Initialization of project structure

---
+++ src/main/java/com/github/smagindmitrus/AccordeON/AccordeOnApplication.java
@@ -0,0 +1,13 @@
+package com.github.smagindmitrus.AccordeON;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccordeOnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccordeOnApplication.class, args);
+	}
+
+}
